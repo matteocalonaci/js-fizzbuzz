3f8e41636ec01a8159828f6f3510b24bce94b389
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-04-22 18:35:23 +0200

    commit 2

--- JS/main.js
+++ JS/main.js
@@ -12,33 +12,53 @@
 
 
 
-
-const myUl = document.querySelector(`ul`);
-
+const item = document.querySelector('ul');
 
 
 
 for (let i = 1; i <= 100; i++) {
-    console.log(`Ora metto in lista` + 1);
-    const newLi = document.createElement(`li`);
-    newLi.append(i);
-    myUl.append(newLi);
-    if (i % 5 == 0 && i % 3 == 0) {
-        console.log(`FizzBuzz`);
-        newLi.append(`FizzBuzz`);
-    } else if (i % 3 === 0) {
-        console.log(`Fizz`);
-        newLi.append(`Fizz`);
-    } else if (i % 5 === 0) {
-        console.log(`Buzz`);
-        newLi.append(`Buzz`);
-    } else {
-        console.log(i);
+    let value = i;
+    let color = "style=background-color:#888888";
+    if (i % 3 == 0 && i % 5 == 0) {
+        value = `FIZZ BUZZ`;
+        color = "style=background-color:blue;"
     }
-
-
+    else if (i % 3 == 0) {
+        value = `FIZZ`;
+        color = "style=background-color:crimson;"
+    }
+    else if (i % 5 == 0) {
+        value = `BUZZ`;
+        color = "style=background-color:#555555;"
+    }
+    item.innerHTML = item.innerHTML + `<li ${color}>${value}</li>`;
 }
 
 
 
+// const myUl = document.querySelector(`ul`);
+
+
+
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(`Ora metto in lista` + 1);
+//     const newLi = document.createElement(`li`);
+//     newLi.append(i);
+//     myUl.append(newLi);
+//     if (i % 5 == 0 && i % 3 == 0) {
+//         console.log(`FizzBuzz`);
+//         newLi.append(`FizzBuzz`);
+//     } else if (i % 3 === 0) {
+//         console.log(`Fizz`);
+//         newLi.append(`Fizz`);
+//     } else if (i % 5 === 0) {
+//         console.log(`Buzz`);
+//         newLi.append(`Buzz`);
+//     } else {
+//         console.log(i);
+//     }
+
+
+// }
 
